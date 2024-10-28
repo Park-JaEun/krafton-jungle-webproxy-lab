be333497950915c888ff3c5440aa5d3325f90f24
ddoavoca <ddoavoca@gmail.com>
2024-10-28 09:40:18 +0900

    csapp 11.10-A 를 위한 adder.c 수정

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -2,27 +2,38 @@
  * adder.c - a minimal CGI program that adds two numbers together
  */
 /* $begin adder */
-#include "csapp.h"
+#include"csapp.h"
 
-int main(void) {
-    char *query_string = getenv("QUERY_STRING"); // URL에서 쿼리 스트링 가져오기
-    int a, b;
+intmain(void){
+ char*buf,*p;
+ char arg1[MAXLINE],arg2[MAXLINE],content[MAXLINE];
+ int n1=0, n2=0;
 
-    /* 스트링 파싱 -> 정수로 분리 */
-    if (query_string == NULL || sscanf(query_string, "%d&%d", &a, &b) != 2) {
-        printf("Content-type: text/html\r\n\r\n");
-        printf("<html><body>");
-        printf("<p>Error: Please provide two integers in the form /adder?a&b</p>");
-        printf("</body></html>");
-        return 1;
-    }
+ /*Extractthetwoarguments*/
+if((buf=getenv("QUERY_STRING")) != NULL){
+ p = strchr(buf,'&');
+ *p = '\0';
+ strcpy(arg1,buf);
+ strcpy(arg2,p+1);
+ n1 = atoi(arg1);
+ n2 = atoi(arg2);
+}
 
-    /* HTTP 응답 헤더 + HTML 응답 본문 출력 */
-    printf("Content-type: text/html\r\n\r\n");
-    printf("<html><body>");
-    printf("<h1>%d + %d = %d</h1>", a, b, a + b);
-    printf("</body></html>");
+ /*Maketheresponsebody*/
+ sprintf(content,"QUERY_STRING=%s",buf);
+ sprintf(content,"Welcome to add.com:");
+ sprintf(content,"%sTHE Internet addition portal.\r\n<p>",content);
+ sprintf(content,"%sThe answer is:%d+%d=%d\r\n<p>",
+ content,n1,n2,n1+n2);
+ sprintf(content,"%sThanks for visiting!\r\n",content);
 
-    return 0;
+ /*GeneratetheHTTPresponse*/
+ printf("Connection: close\r\n");
+ printf("Content-length:%d\r\n",(int)strlen(content));
+ printf("Content-type:text/html\r\n\r\n");
+ printf("%s",content);
+ fflush(stdout);
+
+ exit(0);
 }
 /* $end adder */
